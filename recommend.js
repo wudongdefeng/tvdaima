11dd41db2e9e723132ebc25fab935a907d638a0e
舞动的风 <615563901@qq.com>
2020-03-11 01:16:39 +0800

    update recommend.js.

--- recommend.js
+++ recommend.js
@@ -19,27 +19,27 @@
                 },
                 {
 
-"name": "贝爷荒野",
+"name": "PPTV",
 
-"url": "https://aldirect.hls.huya.com/huyalive/28466698-2689656358-11551986095088467968-3048959616-10057-A-1525510878-1_1500.m3u8"
+"url": "https://m.pptv.com/"
 
 },
 
 {
-          "name": "十万个冷笑话",
-          "url": "http://aldirect.hls.huya.com/huyalive/94525224-2553630828-10967760892317401088-3049003140-10057-A-0-1_1500.m3u8"
+          "name": "B站",
+          "url": "https://www.bilibili.com/"
         },
         {
-          "name": "Big笑工坊",
-          "url": "https://js.hls.huya.com/huyalive/94525224-2669150520-11463914191501393920-4413173724-10057-A-0-1_1500.m3u8"
+          "name": "CCTV",
+          "url": "https://m.cctv.com/"
         },
         {
-          "name": "野食小哥",
-          "url": "https://tx.hls.huya.com/huyalive/96584413-96584413-414826895138357248-3724460598-10057-A-0-1_1500.m3u8"
+          "name": "虎牙直播",
+          "url": "https://m.huya.com/g/2135"
         },
         {
-          "name": "动画电影",
-          "url": "https://aldirect.hls.huya.com/huyalive/29106097-2689406818-11550914328949424128-2789274560-10057-A-1525417691-1_1500.m3u8"
+          "name": "喜马拉雅",
+          "url": "https://m.ximalaya.com/"
         },
         {
           "name": "自定义",
