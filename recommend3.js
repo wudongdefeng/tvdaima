c743660a53f3f3d5a94eb3146f55659fc096e358
舞动的风 <615563901@qq.com>
2020-03-14 14:29:25 +0800

    直播源

---
+++ recommend3.js
@@ -0,0 +1,97 @@
+       {
+            "name": "央视🎈",
+            "lives": [
+                {
+                    "name": "爱奇艺",
+                    "url": "http://www.iqiyi.com"
+                },
+{
+                    "name": "优酷视频",
+                    "url": "https://www.youku.com/"
+                },
+                {
+                    "name": "腾讯视频",
+                    "url": "http://m.v.qq.com"
+                },
+                {
+                    "name": "芒果TV",
+                    "url": "http://m.mgtv.com"
+                },
+                {
+                    "name": "PPTV",
+                    "url": "https://m.pptv.com/"
+                },
+                {
+          "name": "B站",
+          "url": "https://www.bilibili.com/"
+        },
+        {
+          "name": "CCTV",
+          "url": "https://m.cctv.com/"
+        },
+        {
+          "name": "虎牙直播",
+          "url": "https://m.huya.com/g/2135"
+        },
+        {
+          "name": "喜马拉雅",
+          "url": "https://m.ximalaya.com/"
+        },
+        {
+          "name": "乐球吧",
+          "url": "https://m.leqiuba.com"
+        },
+        {
+          "name": "直播吧",
+          "url": "https://m.zhibo8.cc/?from=pc"
+        },
+        {
+          "name": "录像体育",
+          "url": "http://www.luxiangtiyu.com"
+        },
+        {
+          "name": "篮球直播",
+          "url": "http://m.lanqiu.tv"
+        },
+        {
+          "name": "360直播",
+          "url": "http://m.360zbz.com"
+        }, 
+         {
+          "name": "全国直播",
+          "url": "http://bddn.cn/zb.htm"
+        },
+         {
+          "name": "溜溜体育直播",
+          "url": "https://lltyzb.com/client/homem"
+        },
+         {
+          "name": "中国体育直播",
+          "url": "http://www.zhibo.tv/index/index"
+        },
+         {
+          "name": "飞速直播",
+          "url": https://m.feisuzhibo.com"
+        },
+        {
+          "name": "球趣直播",
+          "url": "http://qiuquzhibo.com"
+        },
+        {
+          "name": "爱看球直播",
+          "url": "https://m.aikanqiu.com"
+        },
+        {
+          "name": "柠檬直播",
+          "url": "http://m.ningmengbo.com"
+        },
+        {
+          "name": "JRS直播",
+          "url": "http://m.jrsnba.com"
+        },
+        {
+          "name": "自定义",
+          "url": "http://www.iyuji.cn/iyuji/s/MkJwenVHOEdBVE10L0ZqcmFibnBwZz09/1560953145493391"
+        }
+            ]
+        }
