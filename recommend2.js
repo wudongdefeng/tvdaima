35b1438f4b125737d6422aca59e975769c6573d1
舞动的风 <615563901@qq.com>
2020-03-14 14:31:04 +0800

    update recommend2.js.

--- recommend2.js
+++ recommend2.js
@@ -37,7 +37,58 @@
           "name": "喜马拉雅",
           "url": "https://m.ximalaya.com/"
         },
-                {
+        {
+          "name": "乐球吧",
+          "url": "https://m.leqiuba.com"
+        },
+        {
+          "name": "直播吧",
+          "url": "https://m.zhibo8.cc/?from=pc"
+        },
+        {
+          "name": "录像体育",
+          "url": "http://www.luxiangtiyu.com"
+        },
+        {
+          "name": "篮球直播",
+          "url": "http://m.lanqiu.tv"
+        },
+        {
+          "name": "360直播",
+          "url": "http://m.360zbz.com"
+        }, 
+         {
+          "name": "全国直播",
+          "url": "http://bddn.cn/zb.htm"
+        },
+         {
+          "name": "溜溜体育直播",
+          "url": "https://lltyzb.com/client/homem"
+        },
+         {
+          "name": "中国体育直播",
+          "url": "http://www.zhibo.tv/index/index"
+        },
+         {
+          "name": "飞速直播",
+          "url": https://m.feisuzhibo.com"
+        },
+        {
+          "name": "球趣直播",
+          "url": "http://qiuquzhibo.com"
+        },
+        {
+          "name": "爱看球直播",
+          "url": "https://m.aikanqiu.com"
+        },
+        {
+          "name": "柠檬直播",
+          "url": "http://m.ningmengbo.com"
+        },
+        {
+          "name": "JRS直播",
+          "url": "http://m.jrsnba.com"
+        },        {
           "name": "自定义",
           "url": "http://www.iyuji.cn/iyuji/s/MkJwenVHOEdBVE10L0ZqcmFibnBwZz09/1560953145493391"
         }
